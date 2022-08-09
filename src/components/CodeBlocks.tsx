bdb77b25f9980ec03ef3e8ac433e8d6fbad98b67
Nils Eriksson <nilseriksson89.ne@gmail.com>
2022-08-09 20:03:32 +0200

    decent version 1 for desktop

--- src/components/CodeBlocks.tsx
+++ src/components/CodeBlocks.tsx
@@ -9,6 +9,7 @@ import Prism from "prism-react-renderer/prism";
 // @ts-ignore
 (typeof global !== "undefined" ? global : window).Prism = Prism;
 require("prismjs/components/prism-java")
+require("prismjs/components/prism-kotlin")
 /* ########################################################### */
  
 
--- src/components/Skew.tsx
+++ src/components/Skew.tsx
@@ -1,11 +1,30 @@
 import React from "react"
 import { twMerge } from "tailwind-merge"
 
-export const Skew = ({children, color = "red"}: {children:string, color?: "red" | "white"}) => {
-      const bg = color === "red" ? "bg-red-500" : "bg-white"
-      const textColor = color === "red" ? "text-white" : "text-black"
-      return <div className={`-skew-x-[10deg] px-[12px] py-[5px] ${textColor} ${bg}`}>{children}</div>
-}
+
+export const Skew = ({
+  children,
+  color = "red",
+  className,
+}: {
+  children: React.ReactNode;
+  color?: "red" | "white";
+  className?: string;
+}) => {
+  const bg = color === "red" ? "bg-red-500" : "bg-white";
+  const textColor = color === "red" ? "text-white" : "text-black";
+
+  return (
+    <div
+      className={twMerge(
+        `-skew-x-[10deg] px-[12px] py-[5px] ${textColor} ${bg}`,
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
 
 //Only to be used with Skew
 export const Skewer: React.FC<{children: React.ReactNode, className?: string}> = ({children, className}) => {
@@ -15,5 +34,5 @@ export const Skewer: React.FC<{children: React.ReactNode, className?: string}> =
 }
 
 export const SkewTwo = ({fst, snd, className}: {fst:string, snd:string, className?: string}) => {
-    return (<Skewer className={className}><Skew color ="white">{fst}</Skew><Skew>{snd}</Skew></Skewer>)
+    return (<Skewer ><Skew color ="white" className={twMerge("border-l-4 border-y-4 border-yellow-500 ", className)}>{fst}</Skew><Skew className={twMerge("border-r-4 border-y-4 border-yellow-500",className)}>{snd}</Skew></Skewer>)
 }
