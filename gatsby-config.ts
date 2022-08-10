5102dd6c4609d7bfa53afd1ea4bdfeab28b0563d
Nils Eriksson <nilseriksson89.ne@gmail.com>
2022-08-10 12:25:12 +0100

    add metadata

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,30 +3,46 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Linguistic Antipatterns`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.linguistic-antipatterns.com/`,
+    description: `This is a website to teach you to identify and fix linguistic antipatterns in your code`,
+    image: `/logo_image.png`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [ "gatsby-plugin-sitemap", 
-    "gatsby-plugin-mdx", 
+  plugins: [
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-mdx",
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        "name": "antipatterns",
+        name: "antipatterns",
         path: `${__dirname}/content/antipatterns/`,
-    }},
-  'gatsby-plugin-postcss',
-  {
+      },
+    },
+    "gatsby-plugin-postcss",
+    {
       resolve: `gatsby-transformer-remark`,
       options: {
-        plugins: [
-          `gatsby-remark-smartypants`,
-        ],
-      }
-    }
-   ]
+        plugins: [`gatsby-remark-smartypants`],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Linguistic Antipatterns`,
+        short_name: `Linguistic Antipatterns`,
+        start_url: `/`,
+        background_color: `#e2e8f0`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon/android-chrome-512x512.png`, // This path is relative to the root of the site.
+      },
+    },
+  ],
 };
 
 export default config;
---
+++ src/components/seo.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useSiteMetadata } from "../hooks/use-site-metadata";
+
+type Props = {
+  title?: string;
+  description?: string;
+  pathname?: string;
+  children?: React.ReactNode;
+};
+
+export const SEO = ({ title, description, pathname, children }: Props) => {
+  const {
+    title: defaultTitle,
+    description: defaultDescription,
+    image,
+    siteUrl,
+  } = useSiteMetadata();
+  const seo = {
+    title: title || defaultTitle,
+    description: description || defaultDescription,
+    image: `${siteUrl}${image}`,
+    url: `${siteUrl}${pathname || ``}`,
+  };
+  return (
+    <>
+      <title>{seo.title}</title>
+      <meta name="description" content={seo.description} />
+      <meta name="image" content={seo.image} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={seo.title} />
+      <meta name="twitter:url" content={seo.url} />
+      <meta name="twitter:description" content={seo.description} />
+      <meta name="twitter:image" content={seo.image} />
+      <link rel="icon" href="/icon.png" />
+      {children}
+    </>
+  );
+};
